2fc4fe6266c5b7b9887957fc8035f3b2f31469c6
ADARSH KUMAR <adarshkarwa163@gmail.com>
2024-02-17 03:02:10 +0530

    ConversionDatatypes

---
+++ l5.js
@@ -0,0 +1,42 @@
+//OPERATIONS
+
+let value = 5
+let negvalue = -value
+console.log(negvalue);
+
+/*
+Basic Maths operations
+1. Addition
+2. Substraction
+3. Divison
+4. Multiplication
+5. To the Power
+6. Remainder
+*/
+
+//basic addition of string
+let s1 = "Hello"
+let s2 = " ji"
+let s3 = s1 + s2
+console.log(s3);
+
+// complex addition of Numbers with string with two numbers
+console.log("1" + 2);
+console.log(1 + "2")
+
+// complex addition of Numbers with string with two numbers
+console.log("1" + 2 + 2);
+console.log(1 + "2" + 2)
+console.log("1" + "2" + 2)
+console.log("1" + 2 + "2")
+console.log(1 + 2 + "2")
+ 
+console.log(+true)
+console.log(+"");
+
+//Assignment operator
+let num1, num2, num3 = 2 + 2 // don't write this type of codes
+
+let COD = 100
+COD-- // in some it happens and we get different values
+console.log(COD);
