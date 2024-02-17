c432787d229c9f0e340beee380cf8f28fcab0662
ADARSH KUMAR <adarshkarwa163@gmail.com>
2024-02-18 03:41:32 +0530

    Comparision

---
+++ l6.js
@@ -0,0 +1,22 @@
+console.log(2>1);
+// //Problem occurs when we don't compare the same datatypes
+console.log("2" > 1);
+console.log("02" > 1);
+
+//null comparision
+console.log(null > 0)//
+console.log(null < 0)
+console.log(null == 0)
+console.log(null >= 0) //null treated as 0 in this case
+
+//undefined comparision
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0)
+console.log(undefined <= 0);
+
+//comparsion and equality check are different things
+
+// strict check ===
+console.log("2" === "ada");
